9998a2b341cb308cf85e7a3ec3e5a85160908a11
brian d foy <briandfoy@pobox.com>
2026-03-25 08:28:54 -0400

    max_window is the number of primes, by default

--- fast.pl
+++ fast.pl
@@ -10,12 +10,12 @@ $|++;
 
 my( $max_exponent, $max_window ) = @ARGV;
 $max_exponent //= 13;
-$max_window   //= 3;
 
 my @primes = (
 	2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
 	53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101
 	);
+$max_window //= $#primes;
 
 my @RESULTS;
 
