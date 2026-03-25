d8a6950f8a87d8a0b7d26df98b0788f1ffc7b343
brian d foy <briandfoy@pobox.com>
2026-03-25 03:17:56 -0400

    number the lines so we get the sequence

--- highly_composite_numbers.pl
+++ highly_composite_numbers.pl
@@ -1,23 +1,45 @@
 #!perl
-
 use v5.40;
 use Math::Prime::Util qw(divisors);
 
 $|++;
 
+my( $file ) = @ARGV;
+my( $seq, $n );
+imy $winner_so_far;
+f( open my $fh, '<', $file ) {
+	my $last;
+	$last = $_ while(<$fh>);
+
+	$last = trim($last);
+	( $seq, $n, $winner_so_far ) = split /\s+/, $last;
+	$winner_so_far =~ s/\D//g;
+	say "SEQ: $seq N: $n W: $winner_so_far";
+	}
+else {
+	die "Could not open $file: $!";
+	}
+exit;
+
+open my $out, '>', $file or die "Could not open <$file> for writing: $!";
+print "\n"; # just to ensure we are on a fresh line
+
 $SIG{'INT'} = sub { exit };
-my $winner_so_far;
+
 while(1) {
 	state $n = $ARGV[0] // 1;
 	state $sequence = 1;
 
+	next if( $n < 60 and $n % 10 );
 	my @d = divisors($n);
 	next unless @d > $winner_so_far;
 	$seq++;
 
 	$winner_so_far = @d;
-	say "$seq $n ($winner_so_far): @d";
+
+	say {$out_fh} "$seq $n ($winner_so_far): @d";
 	}
 
 # 897612484786617600
 # 1102701600
+# 128501493120
