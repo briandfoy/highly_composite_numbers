ccf7a88d76744a1b8ce8dcee807da8f41b131f90
brian d foy <briandfoy@pobox.com>
2026-03-25 03:21:16 -0400

    not every file can have the same name

--- highly_composite_numbers.pl
+++ highly_composite_numbers.pl
@@ -20,11 +20,11 @@ if( open my $fh, '<', $file ) {
 else {
 	die "Could not open $file: $!";
 	}
-$seq++;
+$n++;
 
 exit;
 
-open my $out, '>', $file or die "Could not open <$file> for writing: $!";
+open my $out_fh, '>', $file or die "Could not open <$file> for writing: $!";
 print "\n"; # just to ensure we are on a fresh line
 
 $SIG{'INT'} = sub { exit };
@@ -33,7 +33,7 @@ while(1) {
 	state $n = $ARGV[0] // 1;
 	state $sequence = 1;
 
-	next if( $n < 60 and $n % 10 );
+	next if( $n > 60 and $n % 10 );
 	my @d = divisors($n);
 	next unless @d > $winner_so_far;
 	$seq++;
